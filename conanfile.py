40faf4857bb8ef93583a51edc7f0ba1705f8a9ec
bincrafters-user <bincrafters@gmail.com>
2020-05-24 05:00:15 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.24.3

--- conanfile.py
+++ conanfile.py
@@ -54,8 +54,8 @@ class GStPluginsBaseConan(ConanFile):
         self.build_requires("meson/0.54.2")
         if not tools.which("pkg-config"):
             self.build_requires("pkg-config_installer/0.29.2@bincrafters/stable")
-        self.build_requires("bison_installer/3.3.2@bincrafters/stable")
-        self.build_requires("flex_installer/2.6.4@bincrafters/stable")
+        self.build_requires("bison/3.5.3")
+        self.build_requires("flex/2.6.4")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
