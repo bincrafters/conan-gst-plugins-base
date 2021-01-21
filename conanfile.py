6229379cd2855f2905e39845318807406b282f0a
NarcDev <nicolas.braini@arcanes-technology.fr>
2021-01-21 09:58:12 +0100

    test

--- conanfile.py
+++ conanfile.py
@@ -113,7 +113,7 @@ class GStPluginsBaseConan(ConanFile):
 
     def build(self):
         for p in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**p)
+            tools.patch(**p)erer
             
         self._copy_pkg_config("glib")
         self._copy_pkg_config("gstreamer")
